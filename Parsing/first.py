bd5b4ae4299e992dc8a5521e5ce0e6f7b6e14a93
Huntez <osigor5464@gmail.com>
2022-12-01 00:09:30 +0200

    working with requests library and fake useragent

--- Parsing/first.py
+++ Parsing/first.py
@@ -1,32 +1,22 @@
 import requests
+import connections
+from fake_useragent import UserAgent
 
-user_agent = {
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) \
-AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
-    "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7"
-}
+ua = UserAgent()
 
-# query = {
-#     "search_query" : "morning"
-# }
+# geting name for file
+def get_name(file_url):
+    return(file_url.split("/")[-1])
 
-# form = {
-#     "custemail": "kva@kva", 
-#     "custname": "kva", 
-#     "custtel": "kva"
-# }
+# parsing video
+def download_file(url):
+    try:
+        request = requests.get(url, headers={"User-Agent": ua.random}, stream=True)
+        with open(get_name(url), "wb") as webfile:
+            for chunk in request.iter_content(chunk_size=1024 * 100):
+                webfile.write(chunk)
+        return("Download complete!")
+    except Exception as error:
+        print(error)
 
-# r = requests.get('https://httpbin.org/get', headers=user_agent)
-# r = requests.get('https://www.youtube.com/results', headers=user_agent, params=query)
-# print(r.status_code)
-# print(r.text)
-
-url = 'https://www.shutterstock.com/shutterstock/videos/1007443438/\
-preview/stock-footage-switzerland-winter-landscape-aerial-shot-cinematic-flight-over-a-pine-forest-covered-with-snow.webm'
-r = requests.get(url, headers=user_agent, stream=True)
-print(r.raw.read(10))
-
-# with open("1.webm", "wb") as webfile:
-#     for chunk in r.iter_content(chunk_size=1024 * 100):
-#         webfile.write(chunk)
-#         print("write chunk")
+download_file(connections.video_url)
